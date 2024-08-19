9c5a74e2b08bf9afd7481d03fb78584a4278da09
dyddks <dyddks0115@naver.com>
2024-08-19 19:36:48 +0900

    chore: 파일명 수정

---
+++ TodoApp/src/components/CompletedTodo.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    완료한 투두리스트 : {{ numberOfCompletedTodo }}
+  </div>
+</template>
+<script>
+export default{
+  computed :{
+    numberOfCompletedTodo(){
+      return this.$store.getters["todo/numberOfCompletedTodo"];
+    }
+  }
+}
+</script>
