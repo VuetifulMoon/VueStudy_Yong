1cc3c5fc5b6963eb02bbf369f2ea19d3ad81f79f
DESKTOP-KSUP4OT\dyddk <dyddks0115@naver.com>
2024-08-19 17:34:23 +0900

    feat: TodoList와 CompletedList 분리

--- TodoApp/src/components/TodoApp.vue
+++ TodoApp/src/components/TodoApp.vue
@@ -5,7 +5,10 @@
     <AddTodo />
     <SearchTodo />
     <hr/>
-    <TodoList />
+    <div class="flex">
+      <TodoList title="TodoList" :list='todos'/>
+      <TodoList title="CompletedList" :list="completedTodos"/>
+    </div>
   </div>
 </template>
 
@@ -20,8 +23,48 @@ export default {
     TodoList,
     AddTodo,
     CompletedTodo,
-    SearchTodo
+    SearchTodo,
   },
+  computed: {
+    todos() {
+      if (this.$store.state.todo.searchText === '') {
+        const searchResult = [];
+        this.$store.state.todo.todos.map(todo => {
+          if (!todo.checked) {
+            searchResult.push(todo);
+          }
+        })
+        return searchResult;
+      } else {
+        const searchResult = [];
+        this.$store.state.todo.todos.map(todo => {
+          if (todo.text.includes(this.$store.state.todo.searchText) && !todo.checked) {
+            searchResult.push(todo);
+          }
+        })
+        return searchResult;
+      }
+    },
+    completedTodos() {
+      if (this.$store.state.todo.searchText === '') {
+        const searchResult = [];
+        this.$store.state.todo.todos.map(todo => {
+          if (todo.checked) {
+            searchResult.push(todo);
+          }
+        })
+        return searchResult;
+      } else {
+        const searchResult = [];
+        this.$store.state.todo.todos.map(todo => {
+          if (todo.text.includes(this.$store.state.todo.searchText) && todo.checked) {
+            searchResult.push(todo);
+          }
+        })
+        return searchResult;
+      }
+    }
+  }
 }
 </script>
 
--- TodoApp/src/components/TodoList.vue
+++ TodoApp/src/components/TodoList.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
-    <Todo v-for='todo in todos' :key="todo.id" :todo='todo'/>
-    </div>
+    <h2>{{title}}</h2>
+    <Todo v-for='todo in list' :key="todo.id" :todo='todo'/>
+  </div>
 </template>
 
 <script>
@@ -11,19 +12,12 @@ export default {
   components: {
     Todo
   },
-  computed: {
-    todos() {
-      if (this.$store.state.todo.searchText === '') {
-        return this.$store.state.todo.todos;
-      } else {
-        const searchResult = [];
-        this.$store.state.todo.todos.map(todo => {
-          if (todo.text.includes(this.$store.state.todo.searchText)) {
-            searchResult.push(todo);
-          }
-        })
-        return searchResult;
-      }
+  props: {
+    title: {
+      type: String
+    },
+    list: {
+      type: Array
     }
   }
 }
